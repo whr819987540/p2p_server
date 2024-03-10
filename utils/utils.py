ca5c6d3987870b81e67554ac7285c2ffb313dff9
whr819987540 <819987540@qq.com>
2024-03-10 09:29:27 +0800

    FEDAVG_STRATEGY is random client selection

--- utils/utils.py
+++ utils/utils.py
@@ -21,7 +21,7 @@ STANDALONE_MODE = "standalone"
 BTPS_TRANSFER_MODE = "BTPS"
 PS_TRANSFER_MODE = "PS"
 
-FEDAVG_STRATEGY = "fedavg"
+FEDAVG_STRATEGY = "fedavg" # random selection
 OORT_STRATEGY = "oort"
 
 def str2bool(v):
